81a0b00ca4944b185c1e9270078ab5401da24c06
katokdoescode <47973467+katokdoescode@users.noreply.github.com>
2025-06-18 16:36:22 +0400

    Update site link

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://hyperskill.github.io/ai-tutors-example-library',
+	site: 'https://hyperskill.github.io/ai-tutors-example-library/',
 	integrations: [mdx(), sitemap()],
 })
