bc967f217b20ddb475513d5ec0a4a0479a619f8d
katokdoescode <47973467+katokdoescode@users.noreply.github.com>
2025-06-18 16:25:22 +0400

    Fix links

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/examples/${post.id}/`,
+			link: `${context.site}examples/${post.id}/`,
 		})),
 	})
 }
